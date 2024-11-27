e2d71022d5812fae8a9e7881692d1e91ef801617
Edison Suarez <e.suarezd@uniandes.edu.co>
2024-11-27 16:24:47 -0500

    streamlit_app y version sckilearn 1.5.2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 import pandas as pd
 import numpy as np
 
@@ -21,12 +22,22 @@ st.markdown("""
 Bienvenido a la herramienta. Esta aplicación permite localizar contrucciones usando Técnicas supervisadas de Deep Learning. 
 
 ## ¿Cómo funciona?
-1. Suba un archivo CSV con los datos de vuelo de drones.
-2. La aplicación muestra los datos del archivo cargado para hacer la prueba.
-3. De clic en 'Hacer predicciones' para que muestre los resultados. Este mostrará para cada prueba de vuelo ingresada el consumo que tendrá la bateria.
-
-Para comenzar, por favor, sube tu archivo CSV utilizando el botón de abajo.
+1. Suba una imagen satelital de las tomadas en campo.
+2. La aplicación muestra la imagen que se quiere analizar.
+3. De clic en 'localizar' para que muestre los resultados.
 """)
+# Subir la imagen
+uploaded_file = st.file_uploader("Sube una imagen", type=["png", "jpg", "jpeg"])
+
+if uploaded_file is not None:
+    # Leer la imagen usando Pillow
+    image = Image.open(uploaded_file)
+
+    # Mostrar la imagen en la aplicación
+    st.image(image, caption="Imagen subida", use_container_width=True)
+else:
+    st.write("Por favor, sube una imagen para mostrarla.")
+
 # Subir archivo CSV
 uploaded_file = st.file_uploader("Subir archivo CSV", type=["csv"])
 
